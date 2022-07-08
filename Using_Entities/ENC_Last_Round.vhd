f427bf9feab16aa2c928775c10d43884abbf389e
FAdy-200 <fadi.alahmad.alomar@gmail.com>
2022-07-08 17:32:12 +0200

    Made it work with the ENC entity. Needs to be changed when the rest is turned into entities

---
+++ Using_Entities/ENC_Last_Round.vhd
@@ -0,0 +1,32 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use ieee.numeric_std.all;
+use IEEE.STD_LOGIC_UNSIGNED.ALL;
+use work.Shift_Row_Package.all;
+use work.Substitution_Package.all;
+
+
+
+entity ENC_Last_Round is
+port(
+		data_in :in STD_LOGIC_VECTOR (127 downto 0);
+		data_out : out STD_LOGIC_VECTOR (127 downto 0);
+		expanded_key : in STD_LOGIC_VECTOR (127 downto 0)
+		);
+end ENC_Last_Round;
+
+architecture behavior of ENC_Last_Round is
+		signal substitue_out : STD_LOGIC_VECTOR (127 downto 0);
+		signal shift_out : STD_LOGIC_VECTOR (127 downto 0);
+		signal mode : STD_LOGIC;
+begin 		
+				mode <= '0';
+				substitute_all(data_in,mode,substitue_out);
+				shift_rows(substitue_out,shift_out);
+				Add :	entity work.Add(behavior)
+				port map(
+					data_in_1 => shift_out,
+					data_in_2 => expanded_key,
+					data_out => data_out
+					);
+end behavior;
--- Using_Entities/ENC_Round.vhd
+++ Using_Entities/ENC_Round.vhd
@@ -3,7 +3,7 @@ use IEEE.STD_LOGIC_1164.ALL;
 use ieee.numeric_std.all;
 use IEEE.STD_LOGIC_UNSIGNED.ALL;
 use work.Shift_Row_Package.all;
---use work.Mix_Column_Package.all;
+use work.Mix_Column_Package.all;
 use work.Substitution_Package.all;
 
 
@@ -17,12 +17,19 @@ port(
 end ENC_Round;
 
 architecture behavior of ENC_Round is
-		signal temp : STD_LOGIC_VECTOR (127 downto 0);
+		signal substitue_out : STD_LOGIC_VECTOR (127 downto 0);
+		signal shift_out : STD_LOGIC_VECTOR (127 downto 0);
+		signal mix_out : STD_LOGIC_VECTOR (127 downto 0);
 		signal mode : STD_LOGIC;
 begin 		
 				mode <= '0';
-				substitute_all(data_in,mode,temp);
-				--shift_rows(temp,temp);
-				--temp <= temp xor expanded_key;
-				data_out <= temp;
+				substitute_all(data_in,mode,substitue_out);
+				shift_rows(substitue_out,shift_out);
+				mix_columns(shift_out,mix_out);
+				Add :	entity work.Add(behavior)
+				port map(
+					data_in_1 => mix_out,
+					data_in_2 => expanded_key,
+					data_out => data_out
+					);
 end behavior;
