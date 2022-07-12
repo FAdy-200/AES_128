d7c160434545991154277e11d6e6b0c6c95c3f01
FAdy-200 <fadi.alahmad.alomar@gmail.com>
2022-07-12 17:12:18 +0200

    Modified to work with the new key rounds entities.

--- Using_Entities/Key_Expansion.vhd
+++ Using_Entities/Key_Expansion.vhd
@@ -7,16 +7,63 @@ use IEEE.STD_LOGIC_UNSIGNED.ALL;
 entity Key_Expansion is
 port(
 		key :in STD_LOGIC_VECTOR (127 downto 0);
-		expanded_key : out STD_LOGIC_VECTOR (1407 downto 0)
+		expanded_key_out : out STD_LOGIC_VECTOR (1407 downto 0)
 		);
 end Key_Expansion;
 
 architecture behavior of Key_Expansion is
+	signal expanded_key :STD_LOGIC_VECTOR (1407 downto 0);
 begin 		
-				Round_1 :	entity work.Key_Expansion_Round(behavior)
+				Round_1 :	entity work.Key_Expansion_Round_1(behavior)
 				port map(
 					subkey_in => key,
-					subkey_number => 1,
-					subkey_out => expanded_key(1407 downto 1280)
+					subkey_out => expanded_key(1279 downto 1152)
 					);
+				Round_2 :	entity work.Key_Expansion_Round_2(behavior)
+				port map(
+					subkey_in => expanded_key(1279 downto 1152),
+					subkey_out => expanded_key(1151 downto 1024)
+					);
+				Round_3 :	entity work.Key_Expansion_Round_3(behavior)
+				port map(
+					subkey_in => expanded_key(1151 downto 1024),
+					subkey_out => expanded_key(1023 downto 896)
+					);
+				Round_4 :	entity work.Key_Expansion_Round_4(behavior)
+				port map(
+					subkey_in => expanded_key(1023 downto 896),
+					subkey_out => expanded_key(895 downto 768)
+					);
+				Round_5 :	entity work.Key_Expansion_Round_5(behavior)
+				port map(
+					subkey_in => expanded_key(895 downto 768),
+					subkey_out => expanded_key(767 downto 640)
+					);
+				Round_6 :	entity work.Key_Expansion_Round_6(behavior)
+				port map(
+					subkey_in => expanded_key(767 downto 640),
+					subkey_out => expanded_key(639 downto 512)
+					);
+				Round_7 :	entity work.Key_Expansion_Round_7(behavior)
+				port map(
+					subkey_in => expanded_key(639 downto 512),
+					subkey_out => expanded_key(511 downto 384)
+					);
+				Round_8 :	entity work.Key_Expansion_Round_8(behavior)
+				port map(
+					subkey_in => expanded_key(511 downto 384),
+					subkey_out => expanded_key(383 downto 256)
+					);
+				Round_9 :	entity work.Key_Expansion_Round_9(behavior)
+				port map(
+					subkey_in => expanded_key(383 downto 256),
+					subkey_out => expanded_key(255 downto 128)
+					);
+				Round_10 :	entity work.Key_Expansion_Round_10(behavior)
+				port map(
+					subkey_in => expanded_key(255 downto 128),
+					subkey_out => expanded_key(127 downto 0)
+					);
+				expanded_key(1407 downto 1280) <= key;
+				expanded_key_out <= expanded_key;
 end behavior;
