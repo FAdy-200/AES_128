cf692f315f035f0542e0457ddb48784d55075cce
Nawar <abdonawar80@gmail.com>
2022-07-13 20:45:04 +0200

    Finalized DEC rounds, still untested

--- Using_Entities/DEC.vhd
+++ Using_Entities/DEC.vhd
@@ -26,41 +26,70 @@ architecture behavior of DEC is
 		signal data_out_round_9 : STD_LOGIC_VECTOR(127 downto 0);
 begin 	
 
-			Round_0 :	entity work.DEC_Initial_Round(behavior)
+			First_Round :	entity work.DEC_Initial_Round(behavior)
 			port map(
 				data_in => data_in_dec,
 				expanded_key => expanded_key_dec(127 downto 0),
-				data_out_dec => pre_rounds
+				data_out => pre_rounds
 				);
--- package AES_DEC_Package is
-
---   procedure decrypt(signal data_in_dec : in STD_LOGIC_VECTOR (127 downto 0);
--- 										signal expanded_key_dec : in STD_LOGIC_VECTOR (1407 downto 0);
--- 										signal data_out_dec :  out STD_LOGIC_VECTOR (127 downto 0)
---                                     );
-
--- end package;
-
-
--- package body AES_DEC_Package is
-
---   procedure decrypt(signal data_in_dec : in STD_LOGIC_VECTOR (127 downto 0);
--- 										signal expanded_key_dec : in STD_LOGIC_VECTOR (1407 downto 0);
--- 										signal data_out_dec :  out STD_LOGIC_VECTOR (127 downto 0)
---                                     ) is
--- 					variable temp_dec : STD_LOGIC_VECTOR (127 downto 0) := data_in_dec;
--- 					variable mode_dec : STD_LOGIC := '1';
--- 			begin
--- --				temp_dec:= temp_dec xor expanded_key_dec(127 downto 0);
--- --				inv_shift_rows(temp_dec,temp_dec);
--- --				substitute_all(temp_dec,mode_dec,temp_dec);
--- --				for I in 9 downto 1 loop
--- --					temp_dec:= temp_dec xor expanded_key_dec(1407 - I*128 downto 1408 - 128 - I * 128);
--- --					inv_mix_columns(temp_dec,temp_dec);
--- --					inv_shift_rows(temp_dec,temp_dec);
--- --					substitute_all(temp_dec,mode_dec,temp_dec);
--- --				end loop;
--- --				temp_dec:= temp_dec xor expanded_key_dec(1407 downto 1280);
--- --				data_out_dec <= temp_dec;
--- 			end decrypt;
--- end AES_DEC_Package;
+			Round_1 :	entity work.DEC_Round(behavior)
+				port map(
+					data_in => pre_rounds,
+					expanded_key => expanded_key_dec(255 downto 128),
+					data_out => data_out_round_1
+					);
+			Round_2 :	entity work.DEC_Round(behavior)
+				port map(
+					data_in => data_out_round_1,
+					expanded_key => expanded_key_dec(383 downto 256),
+					data_out => data_out_round_2
+					);
+			Round_3 :	entity work.DEC_Round(behavior)
+				port map(
+					data_in => data_out_round_2,
+					expanded_key => expanded_key_dec(511 downto 384),
+					data_out => data_out_round_3
+					);
+			Round_4 :	entity work.DEC_Round(behavior)
+				port map(
+					data_in => data_out_round_3,
+					expanded_key => expanded_key_dec(639 downto 512),
+					data_out => data_out_round_4
+					);
+			Round_5 :	entity work.DEC_Round(behavior)
+				port map(
+					data_in => data_out_round_4,
+					expanded_key => expanded_key_dec(767 downto 640),
+					data_out => data_out_round_5
+					);
+			Round_6 :	entity work.DEC_Round(behavior)
+				port map(
+					data_in => data_out_round_5,
+					expanded_key => expanded_key_dec(895 downto 768),
+					data_out => data_out_round_6
+					);
+			Round_7 :	entity work.DEC_Round(behavior)
+				port map(
+					data_in => data_out_round_6,
+					expanded_key => expanded_key_dec(1023 downto 896),
+					data_out => data_out_round_7
+					);
+			Round_8 :	entity work.DEC_Round(behavior)
+				port map(
+					data_in => data_out_round_7,
+					expanded_key => expanded_key_dec(1151 downto 1024),
+					data_out => data_out_round_8
+					);
+			Round_9 :	entity work.DEC_Round(behavior)
+				port map(
+					data_in => data_out_round_8,
+					expanded_key => expanded_key_dec(1279 downto 1152),
+					data_out => data_out_round_9
+					);
+			Add :	entity work.Add(behavior)
+				port map(
+						data_in_1 => data_out_round_9
+						data_in_2 => expanded_key_dec(1407 downto 1280),
+						data_out => data_out_dec
+						);
+end behavior;
---
+++ Using_Entities/DEC_Round.vhd
@@ -0,0 +1,39 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use ieee.numeric_std.all;
+use IEEE.STD_LOGIC_UNSIGNED.ALL;
+use work.Inv_Shift_Row_Package.all;
+use work.Inv_Mix_Column_Package.all;
+
+
+
+entity DEC_Round is
+port(
+		data_in :in STD_LOGIC_VECTOR (127 downto 0);
+		data_out : out STD_LOGIC_VECTOR (127 downto 0);
+		expanded_key_dec : in STD_LOGIC_VECTOR (127 downto 0)
+		);
+end DEC_Round;
+
+architecture behavior of DEC_Round is
+		signal add_out : STD_LOGIC_VECTOR (127 downto 0);
+        signal mix_out : STD_LOGIC_VECTOR (127 downto 0);
+		signal shift_out : STD_LOGIC_VECTOR (127 downto 0);
+		
+begin
+                Add :	entity work.Add(behavior)
+                port map(
+                    data_in_1 => data_in,
+                    data_in_2 => expanded_key_dec,
+                    data_out => add_out
+                    ); 
+                inv_mix_columns(add_out,mix_out);		
+                inv_shift_rows(mix_out,shift_out);
+				Substitute :	entity work.Substitute_DEC(behavior)
+				port map(
+					data_in => shift_out,
+					data_out => data_out
+					);
+				
+				
+end behavior;
