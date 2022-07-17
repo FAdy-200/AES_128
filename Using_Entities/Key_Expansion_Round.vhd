8ff8a94b79d8912c7d0c9d7e4287438da5b6fd20
FAdy-200 <fadi.alahmad.alomar@gmail.com>
2022-07-18 00:14:30 +0200

    Modified to have the RCON value be given outside of the entity.

--- Using_Entities/Key_Expansion_Round.vhd
+++ Using_Entities/Key_Expansion_Round.vhd
@@ -6,16 +6,15 @@ use work.Key_Expansion_Package.all;
 
 
 
-entity Key_Expansion_Round_1 is
+entity Key_Expansion_Round is
 port(
 		subkey_in :in STD_LOGIC_VECTOR (127 downto 0);
+		rcon_value: in STD_LOGIC_VECTOR (31 downto 0);
 		subkey_out : out STD_LOGIC_VECTOR (127 downto 0)
 		);
-end Key_Expansion_Round_1;
+end Key_Expansion_Round;
 
-architecture behavior of Key_Expansion_Round_1 is
-	signal rcon_value: STD_LOGIC_VECTOR(31 downto 0);
+architecture behavior of Key_Expansion_Round is
 begin 		
-		rcon_value <= x"01000000";
 		expand_round(subkey_in,rcon_value,subkey_out);
 end behavior;
